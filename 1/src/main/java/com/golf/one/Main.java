5efeb4d1fd031e22f295e1829b4bd8220da36c76
Jeff Normile <normile01@allegheny.edu>
2024-01-22 14:05:33 -0500

    First stab

--- 1/src/main/java/com/golf/one/Main.java
+++ 1/src/main/java/com/golf/one/Main.java
@@ -2,8 +2,8 @@ package com.golf.one;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+  public static void main(String[] a) {
+    System.out.print("Hello, World!");
   }
 
 }
