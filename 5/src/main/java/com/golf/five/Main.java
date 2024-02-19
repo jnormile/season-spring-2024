0573b8fb1c9dd9c8b187c513b07286ae8e2d0be0
Jeff Normile <normile01@allegheny.edu>
2024-02-19 11:24:37 -0500

    public no mo

--- 5/src/main/java/com/golf/five/Main.java
+++ 5/src/main/java/com/golf/five/Main.java
@@ -1,1 +1,1 @@
-package com.golf.five;class Main{public static void main(String[]a){String b=a[0];int s=0;for(int i=0;i<10;i++){int d=b.charAt(i);int g=(d=='X')?10:(d-48);s+=(10-i)*g;}System.out.println(s%11==0);}}
+package com.golf.five;class Main{static void main(String[]a){String b=a[0];int s=0;for(int i=0;i<10;i++){int d=b.charAt(i);int g=(d=='X')?10:(d-48);s+=(10-i)*g;}System.out.println(s%11==0);}}
