22d8763717861f946c4fb134f98fc247da7bbd17
Jeff Normile <normile01@allegheny.edu>
2024-02-19 11:18:22 -0500

    one more chop; handle last digit in for loop

--- 5/src/main/java/com/golf/five/Main.java
+++ 5/src/main/java/com/golf/five/Main.java
@@ -1,1 +1,1 @@
-package com.golf.five;class Main{static void main(String[]a){String b=a[0];int s=0;for(int i=0;i<9;i++){int d=b.charAt(i)-48;s+=(10-i)*d;}char r=b.charAt(9);int g=(r=='X')?10:(r-48);System.out.println((s+g)%11==0);}}
+package com.golf.five;class Main{public static void main(String[]a){String b=a[0];int s=0;for(int i=0;i<10;i++){int d=b.charAt(i);int g=(d=='X')?10:(d-48);s+=(10-i)*g;}System.out.println(s%11==0);}}
